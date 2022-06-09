8052cab662521223764a8bd62ccb3cd8eed3b9f9
SarahRazzaq <sarah.razzaq@stud.sundsgarden.se>
2022-06-09 13:49:20 +0200

    last commit

--- script.js
+++ script.js
@@ -1,79 +1,68 @@
-window.onload = function atm(){
-     // parseFloat converts a string to a floating-point number.
-    const menu = parseFloat(prompt("Select a choice. 1.) See balance 2.) Make a deposit 3.) Make a withdrawal 4.) Get account name 5.) Exit Account"));
-    switch(menu) {
-        case 1:
-            alert("Balance: " + account.getBalance());
-            break;
-        case 2:
-            account.deposit();
-            break;
-        case 3: 
-            account.withdrawal();
-            break;
-        case 4:
-            alert("Name of account: " + account.getAccountName());
-            break;
-        case 5:
-            account.exitAccount();
-            function closeWin(){
-                myWindow.close();
-            }
-            break;
-        default:
-            alert("Please enter a valid menu option, number between 1-5.");
-    }
-    atm();
-}
-const account = {
-    accountName: "Sarah Razzaq",
-    balance: 100,
-    getBalance: function () {
-        return this.balance
-    },
-    /*
-    deposit: function () {
-        const amount = parseFloat (prompt("How much would you like to deposit?"));
-        if (!this.accountError(amount)){
-            this.balance = this.balance + amount
-            alert("New balance: " + this.balance)
-        }
-    },
-    withdrawal: function () {
-        const amount = parseFloat (prompt("How much would you like to deposit?"));
-        return (this.balance = this.balance - amount),
-            alert("New balance " + this.balance);
+let accountName = "Sarah Razzaq",
+    balance = 100;
+    
 
-    },
-    */
-    deposit: function () {
-        const amount = parseFloat (prompt("How much would you like to deposit?"));
-        if (!this.accountError(amount)){
-            this.balance = this.balance + amount
-            alert("New balance: " + this.balance)
+    function getBalance() {
+        alert('Your current balance is: '+ balance);
+        atm();
+    }
+    function deposit() {
+        let depositAmount = parseFloat(prompt('How much would you like to deposit?'));
+        if (isNaN(depositAmount) || depositAmount === ''|| depositAmount === 0 || depositAmount < 0) {
+            alert('Error: please enter a valid input!');
+            deposit();
+        } else {
+            balance += depositAmount;
+            getBalance();
+            atm();
         }
-    },
-    withdrawal: function () {
-        const amount = parseFloat (prompt("How much would you lke to withdraw?"));
-        if (!this.accountError(amount) && amount < this.balance) {
-            this.balance = this.balance - amount;
-            alert("New balance " + this.balance)
+    }
+    function withdrawal() {
+        let amount = parseFloat(prompt('How much would you like to deposit?'));
+        if (isNaN(amount) || amount === ''|| amount === 0 || amount < 0 || amount > balance ) {
+            alert('Error: please enter a valid input!');
+            withdrawal();
+        } else {
+            balance -= amount;
+            getBalance();
+            atm();
         }
-    },
-    getAccountName: function () {
-        return this.accountName
-    },
-    accountError: function (amount) {
-        const hasErrors = amount < 0 || isNaN(amount); 
-        if (hasErrors) {
-            alert.error("Amount " + amount + " is invalid. Please enter a valid amount.");
-        }
-        return hasErrors;
-    },
-    extAccount: function (){
+    }
+    	
+    function getAccountName () {
+        alert("Name of account: " +accountName)
+        atm();
+    }
+    function error() {
+        alert('Error: accepted numbers are 1 through 4.');
+        atm();
+    }
+
+    function exit() {
         let myWindow =
         function closeWin() {
         myWindow.close();
+        };
+    }
+
+
+    function atm() {
+
+        var choice = parseInt(prompt('Select a choice 1.) Balance 2.) Deposit 3.) Withdrawal 4.) Get Account Name  5.) Exit')); 
+
+        if (choice === 1) {
+            getBalance();
+        } else if (choice === 2) {
+            deposit();
+        } else if (choice === 3) {
+            withdrawal();
+        } else if (choice === 4) {
+            getAccountName();
+        } else if (choice === 5) {
+            exit();
+        } else {
+            error();
         }
     }
-}
+
+atm();
